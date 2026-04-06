2794beccf73b892a70ee074d5dff33ff171a0783
youduo <youduo.blvms.945@gmail.com>
2026-04-06 17:13:15 +0900

    商品詳細表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :items, only: [:new, :create, :index]
+  resources :items, only: [:new, :create, :index, :show]
   root to: 'items#index'
   
 end
